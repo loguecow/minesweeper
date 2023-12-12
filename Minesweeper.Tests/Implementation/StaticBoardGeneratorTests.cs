89eea205ce23a84e0302b1308e33b58267657a05
Ronny Birkeli <ronny.birkeli@digdir.no>
2023-12-12 22:15:09 +0100

    Extended test set.

--- Minesweeper.Tests/Implementation/StaticBoardGeneratorTests.cs
+++ Minesweeper.Tests/Implementation/StaticBoardGeneratorTests.cs
@@ -26,19 +26,18 @@ public class StaticBoardGeneratorTests
         // Assert
         board.Columns.Should().Be(7);
         board.Rows.Should().Be(7);
+        board.Mines.Should().Be(10);
     }
 
     [Fact]
     public void GenerateBoard_ValidBoardDefinition_ShouldReturnValidBoardWithMines()
     {
-        // Arrange
-        string boardDefinition = ".......,..mm...,......m,m...m..,..m.m..,.mmm...,";
-        // Act
+        string boardDefinition = ".......,..mm...,......m,m...m..,..m.m..,.mmm...,.......";
+
         IBoardGenerator boardGenerator = new StaticBoardGenerator(boardDefinition);
         Board board = boardGenerator.GenerateBoard();
 
-        // Assert
-        board.Tiles[2, 3].IsMine.Should().BeTrue();
+        board.Tiles[1, 3].IsMine.Should().BeTrue();
         board.Tiles[0, 0].IsMine.Should().BeFalse();
         board.Mines.Should().Be(10);
     }
@@ -46,26 +45,48 @@ public class StaticBoardGeneratorTests
     [Fact]
     public void GenerateBoard_InvalidMissingColumn_ShouldThrowException()
     {
-        // Arrange
         string boardDefinition = ".......,..mm...,......m,m...m..,..m.m..,.mmm...,......,";
 
-        // Act
-        IBoardGenerator boardGenerator = new StaticBoardGenerator(boardDefinition);
+        Action newStaticBoardGenerator = () => new StaticBoardGenerator(boardDefinition);
 
-        // Assert
-        boardGenerator.Invoking(bg => bg.GenerateBoard()).Should().Throw<ArgumentException>();
+        newStaticBoardGenerator.Should().Throw<ArgumentException>().WithMessage("*must have equal amount of columns*");
     }
 
     [Fact]
     public void GenerateBoard_InvalidNoMines_ShouldThrowException()
     {
-        // Arrange
         string boardDefinition = ".......,.......,.......,.......,.......,.......,.......";
 
-        // Act
-        IBoardGenerator boardGenerator = new StaticBoardGenerator(boardDefinition);
+        Action newStaticBoardGenerator = () => new StaticBoardGenerator(boardDefinition);
 
-        // Assert
-        boardGenerator.Invoking(bg => bg.GenerateBoard()).Should().Throw<ArgumentException>();
+        newStaticBoardGenerator.Should().Throw<ArgumentException>().WithMessage("*must have at least one mine*");
+    }
+
+    [Fact]
+    public void GenerateBoard_InvalidNoBoardDefinition_ShouldThrowException()
+    {
+        Action newStaticBoardGenerator = () => new StaticBoardGenerator("");
+
+        newStaticBoardGenerator.Should().Throw<ArgumentException>().WithMessage("*cannot be null or empty*");
+    }
+
+    [Fact]
+    public void GenerateBoard_InvalidTooFewRows_ShouldThrowException()
+    {
+        string boardDefinition = ".......,..mm...";
+
+        Action newStaticBoardGenerator = () => new StaticBoardGenerator(boardDefinition);
+
+        newStaticBoardGenerator.Should().Throw<ArgumentException>().WithMessage("*must have at least three rows*");
+    }
+
+    [Fact]
+    public void GenerateBoard_InvalidTooFewColumns_ShouldThrowException()
+    {
+        string boardDefinition = "..,.m,..";
+
+        Action newStaticBoardGenerator = () => new StaticBoardGenerator(boardDefinition);
+
+        newStaticBoardGenerator.Should().Throw<ArgumentException>().WithMessage("*must have at least three columns*");
     }
 }
