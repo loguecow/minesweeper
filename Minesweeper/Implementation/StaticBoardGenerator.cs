6ca866a88664dc22bf422397b01f6c5fb84c0bfc
Andreas Bjørnå <loguecow@protonmail.com>
2024-01-08 09:21:51 +0100

    ShouldReturnValidBoard working

--- Minesweeper/Implementation/StaticBoardGenerator.cs
+++ Minesweeper/Implementation/StaticBoardGenerator.cs
@@ -13,14 +13,28 @@ public class StaticBoardGenerator : IBoardGenerator
     /// </summary>
     /// <param name="boardDefinition">A string that defines the board.</param>
     /// <example>.......,..mm...,......m,m...m..,..m.m..,.mmm...,.......</example>
+    private readonly string[] _rows;
+    private readonly int _columns;
+    private readonly int _minesCount;
+    private readonly string _boardDefinition;
+
+
     public StaticBoardGenerator(string boardDefinition) 
     {
+
+        _boardDefinition = boardDefinition;
         // TODO: Implement this constructor
+        _rows = _boardDefinition.Split(',');
+        _minesCount = _boardDefinition.Count(c => c == 'm');
+
+        
+
     }
     public Board GenerateBoard()
     {
-        //TODO: Implement this method
+        int rowsCount = _rows.Length;
+        int columnsCount = _rows[0].Length;
 
-        return new Board(0, 0, 0);
+        return new Board(rowsCount, columnsCount, _minesCount);
     }
 }
