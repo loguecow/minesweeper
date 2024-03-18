25547e5c36d38725192f4b70dd1f9b54c2f3a078
Andreas Bjørnå <loguecow@protonmail.com>
2024-03-18 14:13:22 +0100

    game logic

--- Minesweeper/Game.cs
+++ Minesweeper/Game.cs
@@ -8,30 +8,45 @@ public class Game
     public DateTime? StartTime { get; private set; } = null;
     public DateTime? EndTime { get; private set; } = null;
     public IPlayer Player { get; set; }
-    public Board Board { get; set; }
+
+    private bool IsFirstTileRevealed { get; set; } = false;
+    private Board _board { get; set; }
 
     public Game(IBoardGenerator boardGenerator, IPlayer player)
     {
-        throw new NotImplementedException();
+        _board = boardGenerator.GenerateBoard();
+        Player = player;
+    }
+    public void RevealTile_FirstTile_ShouldStartTimer()
+    {
+        StartTime = DateTime.Now;
     }
 
     public void RevealTile(int row, int column)
     {
-        throw new NotImplementedException();
+        if (IsFirstTileRevealed == false)
+        {
+            RevealTile_FirstTile_ShouldStartTimer();
+            IsFirstTileRevealed = true;
+        }
+        if (_board.Tiles[row, column].IsMine)
+        {
+            EndTime = DateTime.Now;
+            throw new MineExplodedException();
+        }
+        _board.RevealTile(row, column);
     }
 
     public void FlagTile(int row, int column)
     {
-        throw new NotImplementedException();
+        _board.FlagTile(row, column);
     }
-
-    public double GetSecondsUsed()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void UnflagTile(int row, int column)
-    {
-        throw new NotImplementedException();
-    }
+ 
+public double GetSecondsUsed()
+{
+        DateTime now = DateTime.Now;
+        DateTime _startTime = StartTime ?? DateTime.MinValue;
+        double seconds = (now - _startTime).TotalSeconds;
+        return seconds;
+}
 }
